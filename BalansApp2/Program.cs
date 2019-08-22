660ab12272962ae3c70977b5a71d4cc8ac2bb1fc
Misir Askerov <misirua@code.edu.az>
2019-08-22 21:31:58 +0400

    2-ci tapsiriq final

--- BalansApp2/Program.cs
+++ BalansApp2/Program.cs
@@ -8,77 +8,118 @@ namespace BalansApp2
 {
     class Program
     {
-        static void Main(string[] args)
+        class Person
         {
-        Label1:
-            Console.WriteLine("Adinizi daxil edin: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Soyanidizi daxil edin: ");
-            string surname = Console.ReadLine();
-            Console.WriteLine("Sexsiyyet Vesiqesinin seriyasi");
-            string IdNumber = Console.ReadLine();
+            public string Name { get; private set; }
+            public string Surname { get; private set; }
+            public string Id { get; private set; }
+            public double Amount { get; private set; }
 
-        Label:
-            Console.WriteLine("1 - Balansa bax");
-            Console.WriteLine("2 - Balans artir");
-            Console.WriteLine("3 - Balansdan cixaris");
-            Console.WriteLine("4 - Hesab melumatlarini deyis");
-            Console.WriteLine("0 - Cixis");
-
-            string balansBax = Console.ReadLine();
-
-            double mebleg;
-
-            /* if (Convert.ToInt32(balansBax) == 1)
-             {
-                 Console.WriteLine("balansiniz 0");
-             }
-             if (Convert.ToInt32(balansBax) == 2)
-             {
-                 Console.WriteLine("Mebleg daxil edin: ");
-                 mebleg = Convert.ToDouble(Console.ReadLine());
-
-                 goto Label; 
-             }
-             if (Convert.ToInt32(balansBax) == 3)
-             {
-                 Console.WriteLine("Mebleg daxil edin: ");
-                 double cixilanMebleg = Convert.ToDouble(Console.ReadLine());
-                 if(mebleg - cixilanMebleg < 0)
-                 {
-                     Console.WriteLine("Balansinizda kifayet qeder vesait yoxdur");
-                 }
-             } */
-            switch (Convert.ToInt32(balansBax))
+            public Person(string name, string surname, string id)
             {
-                case 1:
-                    Console.WriteLine("balansiniz 0");
-                    break;
-                case 2:
-                    Console.WriteLine("Mebleg daxil edin: ");
-                    mebleg = Convert.ToDouble(Console.ReadLine());
-                    goto Label;
-                    break;
-                case 3:
-                    Console.WriteLine("Mebleg daxil edin: ");
-                    double cixilanMebleg = Convert.ToDouble(Console.ReadLine());
-                    if (cixilanMebleg < 0)
-                    {
-                        Console.WriteLine("Balansinizda kifayet qeder vesait yoxdur");
-
-                    }
-                    goto Label;
-                    break;
-                case 4:
-                    goto Label1;
-                    break;
-                case 0:
-                    Environment.Exit(0);
-                    break;
+                Name = name;
+                Surname = surname;
+                Id = id;
             }
 
+            public string IncreaseAmount(double count)
+            {
+                Amount += count;
+                return "Mebleg artirildi";
+            }
 
-            Console.ReadKey();
+            public string DecreaseAmount(double count)
+            {
+                if (count <= Amount)
+                {
+                    Amount -= count;
+                    return "Mebleg cixildi";
+                }
+                else
+                {
+                    return "balansinda kifayrt qeder mebleg yoxdur";
+                }
+            }
+        }
+
+        static string ChangePersonInfo(ref Person person)
+        {
+            Console.WriteLine("Adinizi daxil edin: ");
+            string name = Console.ReadLine();
+            if (String.IsNullOrEmpty(name)) return "Empty name";
+
+            Console.WriteLine("Soyadinizi daxil edin: ");
+            string surname = Console.ReadLine();
+            if (String.IsNullOrEmpty(surname)) return "Empty surname";
+
+            Console.WriteLine("Sexsiyyet vesiqesinin seriya nomresin daxil et");
+            string IdNumber = Console.ReadLine();
+            if (String.IsNullOrEmpty(IdNumber)) return "Empty IdNumber";
+
+            person = new Person(name, surname, IdNumber);
+
+            return "Ok";
+        }
+
+        static void Main(string[] args)
+        {
+            Person person = null;
+            bool running = true;
+
+            while (running)
+            {
+                if (person == null)
+                {
+                    Console.WriteLine(ChangePersonInfo(ref person));
+                }
+                else
+                {
+                    Console.WriteLine("1 - Balansa bax");
+                    Console.WriteLine("2 - Balans artir");
+                    Console.WriteLine("3 - Balansdan cixaris");
+                    Console.WriteLine("4 - Hesab melumatlarin deyis");
+                    Console.WriteLine("0 - cixis");
+
+                    string balansBax = Console.ReadLine();
+
+                    switch (Convert.ToInt32(balansBax))
+                    {
+                        case 1:
+                            Console.WriteLine("balans " + person.Amount);
+                            break;
+                        case 2:
+                            Console.WriteLine("Mebleg daxil et: ");
+                            if (Double.TryParse(Console.ReadLine(), out double amount))
+                            {
+                                Console.WriteLine(person.IncreaseAmount(amount));
+                            }
+                            else
+                            {
+                                Console.WriteLine("Parse error");
+                            }
+
+                            break;
+                        case 3:
+                            Console.WriteLine("Mebleg daxil et: ");
+                            if (Double.TryParse(Console.ReadLine(), out amount))
+                            {
+                                Console.WriteLine(person.DecreaseAmount(amount));
+                            }
+                            else
+                            {
+                                Console.WriteLine("Parse error");
+                            }
+
+                            break;
+                        case 4:
+                            Console.WriteLine(ChangePersonInfo(ref person));
+                            break;
+                        case 0:
+                            running = false;
+                            break;
+                    }
+                }
+            }
         }
     }
 }
