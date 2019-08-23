fa1b23942cdc08956cf6dacd84be9425fbe078d1
Misir Askerov <misirua@code.edu.az>
2019-08-23 18:16:40 +0400

    finish

---
+++ array & method/listApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace listApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reqemler daxil edin: ");
+            string [] str = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] nums = new int[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                nums[i] = int.Parse(str[i]);
+
+                
+            }
+            Console.WriteLine("En boyuk reqem: ");
+            Console.WriteLine(nums.Max());
+            Console.WriteLine("En kicik reqem: ");
+            Console.WriteLine(nums.Min());
+            Console.WriteLine("Reqemlerin cemi: ");
+            Console.WriteLine(nums.Sum());
+            Console.WriteLine("Reqemlerin tersi");
+            Array.Reverse(nums);
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
